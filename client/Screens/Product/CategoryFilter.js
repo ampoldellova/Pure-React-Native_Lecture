703cf0dfb31ea7349e3a5b5eac0d010a4eec3c1c
ampoldellova <dellova36@gmail.com>
2024-01-24 09:46:45 +0700

    Category Menu

--- client/Screens/Product/CategoryFilter.js
+++ client/Screens/Product/CategoryFilter.js
@@ -11,12 +11,34 @@ const CategoryFilter = (props) => {
         >
             <VStack space={4} divider={<Divider />} w="100%">
                 <HStack justifyContent="space-between">
-                    <TouchableOpacity key={1} >
-                        <Badge style={[styles.center, { margin: 4 }]}>
-                            <Text style={{ color: 'black' }}>name</Text>
+                    <TouchableOpacity
+                        key={1}
+                        onPress={() => {
+                            props.categoryFilter('all'), props.setActive(-1)
+                        }} >
+                        <Badge style={[styles.center, { margin: 4 },
+                        props.active === -1 ? styles.active : styles.inactive]} colorScheme="info" >
+                            <Text style={{ color: 'black' }}>all</Text>
                         </Badge>
-
                     </TouchableOpacity>
+                    {props.categories.map((item) => (
+                        <TouchableOpacity
+                            key={item.$_id}
+                            onPress={() => {
+                                props.categoryFilter(item.$oid),
+                                    props.setActive(props.categories.indexOf(item))
+                            }}
+                        >
+                            <Badge
+                                style={[styles.center,
+                                { margin: 5 },
+                                props.active == props.categories.indexOf(item) ? styles.active : styles.inactive
+                                ]}
+                            >
+                                <Text style={{ color: 'white' }}>{item.name}</Text>
+                            </Badge>
+                        </TouchableOpacity>
+                    ))}
                 </HStack>
             </VStack>
 
